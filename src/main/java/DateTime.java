0a7a9d256e4f0cef0e1dd5ed1afebcb376d41535
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-08-01 18:04:13 -0300

    JAVA

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,44 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTime {
+    public static void main(String[] args) {
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        LocalTime agora = LocalTime.now();
+        System.out.println(agora);
+
+        LocalDateTime dataHoraAtual = LocalDateTime.now();
+        System.out.println(dataHoraAtual);
+        System.out.println("Ano: " + dataHoraAtual.getYear());
+        System.out.println("Mês: " + dataHoraAtual.getMonth());
+        System.out.println("Dia do mês: " + dataHoraAtual.getDayOfMonth());
+        System.out.println("Dia da semana: " + dataHoraAtual.getDayOfWeek());
+
+        int ano = 2023;
+        int mes = 8;
+        int dia = 1;
+        LocalDate data = LocalDate.of(2024, mes,dia);
+
+        if (hoje.isAfter(data)) {//tbm tem .isBefore
+            System.out.println("A data "+ data + " já passou");
+        } else {
+            System.out.println("A data "+ data + " ainda não passou");
+        }
+
+        DateTimeFormatter formatoBr = DateTimeFormatter.ofPattern("EE/MM/yyyy HH:mm:ss");
+        //yy -> 23 ao inves de 2023
+        //MMM -> mês por extenso abreviado
+        //MMMM -> mês por extenso
+        //EE no lugar de dd -> dia da semana
+        String dataHoraAtualFormatada = dataHoraAtual.format(formatoBr);
+        System.out.println(dataHoraAtualFormatada);
+
+        String dataString = "2023-07-14";
+        LocalDate dataValida = LocalDate.parse(dataString); // instância
+        System.out.println(dataValida);
+    }
+}
---
+++ src/main/java/Listas.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Listas {
+    public static void main(String[] args) {
+        // Lista é flexível, diferente dos vetores
+
+        // generic -> <String>
+        List<Integer> lista = new ArrayList<Integer>();
+        lista.add(9);
+        lista.add(4);
+        lista.add(5);
+        lista.add(8);
+
+        System.out.println(lista.get(2));
+        lista.remove(2);
+        System.out.println(lista.get(2));
+        lista.add(2,5);
+        System.out.println(lista.get(2));
+
+        System.out.println();
+
+        for (int i = 0; i < lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+
+        System.out.println();
+
+        for (Integer numero : lista) {
+            System.out.println(numero);
+        }
+
+        List<String> pessoas = new ArrayList<String>(List.of("Nick", "Alvi", "Bella", "Hope", "Tony"));
+        Collections.sort(pessoas);
+
+        for (String pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+    }
+}
